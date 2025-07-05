abb85dc3a96a58601501df1f53ac7de415bb9bdc
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-05 13:41:10 +0400

    Added recursive implementation of binary search

--- LeetCode/704.cpp
+++ LeetCode/704.cpp
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/binary-search/
-// Attempt 1: Binary search
+// Attempt 1: Binary search (iterative)
 // Outcome: Accepted
 class Solution {
 public:
@@ -20,5 +20,26 @@ public:
             }
         }
         return index;
+    }
+};
+// Attempt 2: Binary Search (recursive)
+// Outcome: Accepted
+class Solution {
+public:
+    int binSearch(vector<int> nums, int low, int high, int target){
+        if (low > high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if (nums[mid] == target){
+            return mid;
+        }
+        else if (nums[mid] > target){
+            return binSearch(nums, low, mid-1, target);
+        }
+        return binSearch(nums, mid+1, high, target);
+    }
+    int search(vector<int>& nums, int target) {
+        return binSearch(nums, 0, nums.size() - 1, target);
     }
 };

80f81bacdb4e912a0f7c70cd6936069813d7726b
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-08 05:05:50 +0400

    Completed question to find the kth missing positive number.
    Involved binary search to find the nearest number in array from which we can find the missing number.
    
    Also learnt about importance of order of conditional statements.

---
+++ LeetCode/1539.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/kth-missing-positive-number/description/
+// Attempt 1: Binary search on hashmap containing missing number count at any index
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0, high = arr.size() - 1;
+        while (low <= high){
+            int mid = (low+high)/2;
+            int missing = arr[mid] - (mid+1);
+            if (missing < k){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return high+k+1;
+    }
+};

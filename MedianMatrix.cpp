b7ccd4944e7fe879338e2426d82bb99ef75d6868
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-16 16:17:09 +0530

    Incomplete program on median matrix and solved question on removing outer parenthesis

---
+++ MedianMatrix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+int median(std::vector<std::vector<int>> arr){
+    int count = 0;
+    for (int i = 0; i < arr.size(); ++i){
+        int low = 0, high = arr.size() - 1;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            if (arr[mid] >)
+        }
+    }
+}
+int main(){
+    int n, num = -1;
+    std::vector<std::vector<int>> arr;
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < n; ++j){
+            std::cin >> num;
+            arr[i].push_back(num);
+        }
+    }
+
+}

d58077a51f4d773f6cf9284d2de825ccc81be980
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-18 02:35:34 +0530

    Renamed files and solved reversing linked list question using recursion

--- LeetCode/206.cpp
+++ LeetCode/206.cpp
@@ -1,48 +1,62 @@
-// https://leetcode.com/problems/rotate-string/description/
-// Attempt 1: Brute force
+// https://leetcode.com/problems/reverse-linked-list/description/
+// Attempt 1: Change links, iterative
 // Outcome: Accepted
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    string rotate(string s){
-        char temp = s[0];
-        for (int i = 0; i < s.size() - 1; ++i){
-            s[i] = s[i+1];
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* next = head; //can be initalized to nullptr
+        while (curr != nullptr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        s[s.size() - 1] = temp;
-        return s;
-    }
-    bool rotateString(string s, string goal) {
-        for (int i = 0; i < s.size(); ++i){
-            if (s == goal){
-                return true;
-            }
-            s = rotate(s);
-        }
-        return false;
+        return prev;
     }
 };
-// Attempt 2: Using find and making a string which has all rotations of goal
+// Attempt 2: Recursion
 // Outcome: Accepted
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    string rotate(string s){
-        char temp = s[0];
-        for (int i = 0; i < s.size() - 1; ++i){
-            s[i] = s[i+1];
-        }
-        s[s.size() - 1] = temp;
-        return s;
-    }
-    // abcdeabcd
-    bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()){
-            return false;
-        }
-        s += s;
-        if (s.find(goal) != string::npos){
-            return true;
-        }
-        return false;
+    // ListNode* reverse(ListNode* head){
+    //     if (head->next == nullptr || head == nullptr){
+    //         return head;
+    //     }
+    //     ListNode* temp = reverse(head.next);
+    //     head->next->next = head;
+    //     head->next = nullptr;
+    //     return temp;
 
+    // }
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr){
+            return head;
+        }
+        ListNode* temp = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return temp;
     }
 };

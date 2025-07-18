7afe864ea568c2fb395e4d8b982fd70e8106f26b
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-18 21:27:58 +0530

    Completed question on odd even linked list by creating a new linked list for even indices and then linking them at the end

--- LeetCode/328.cpp
+++ LeetCode/328.cpp
@@ -40,3 +40,43 @@ public:
         return head;
     }
 };
+// Attempt 2: Changing links in every iteration to create two separate linked lists and then linking them in the end
+// Outcome: Accepted
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !(head->next) || !(head->next->next)){
+            return head;
+        }
+        ListNode* temp = head;
+        ListNode* head2 = head->next;
+        ListNode* temp1 = head2;
+        temp->next = temp->next->next;
+        temp = temp->next;
+        while (temp != nullptr && temp->next != nullptr){
+            temp1->next = temp->next;
+            temp1 = temp1->next;
+            if (temp->next->next == nullptr){
+                temp->next = head2;
+                break;
+            }
+            else{
+                temp->next = temp->next->next;
+            }
+            temp = temp->next;
+        }
+        temp1->next = nullptr;
+        temp->next = head2;
+        return head;
+    }
+};

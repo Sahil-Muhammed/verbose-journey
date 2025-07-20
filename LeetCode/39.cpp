4df0b6dc07a0ce77e384e79e4bd43c231aab080a
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-21 03:02:19 +0530

    Implemented a solution involving recursion to the combination sum problem with a sum parameter

--- LeetCode/39.cpp
+++ LeetCode/39.cpp
@@ -26,3 +26,34 @@ public:
         return ans;
     }
 };
+// Attempt 2: Using recursion like previous method, but with a sum parameter as well
+// Outcome: Accepted
+class Solution {
+public:
+    void recursiveTree(vector<int> candidates, vector<int> ds, vector<vector<int>>& ans, int pos, int sum, int target){
+        if (pos == candidates.size()){
+            if (target == sum){
+                (ans).push_back(ds);
+            }
+            return;
+        }
+
+        if (sum > target){
+            return;
+        }
+
+        if ((sum + candidates[pos]) <= target){
+            ds.push_back(candidates[pos]);
+            recursiveTree(candidates, ds, ans, pos, sum + candidates[pos], target);
+            ds.pop_back();
+        }
+
+        recursiveTree(candidates, ds, ans, pos+1, sum, target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        recursiveTree(candidates, ds, ans, 0, 0, target);
+        return ans;
+    }
+};

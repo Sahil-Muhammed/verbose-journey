f285d8e9693d933752f0eb76afc2529d81ba4093
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-20 14:02:30 +0530

    Completed question on implementing atoi using recursion

--- LeetCode/8.cpp
+++ LeetCode/8.cpp
@@ -36,3 +36,65 @@ public:
         return (int)num*minus;
     }
 };
+// Attempt 2: Using recursion
+// Outcome: Accepted
+class Solution {
+public:
+    int recursiveAtoi(string s, int len, int* sign, int* num, int reading){
+        if (len == 0){
+            return (*num)*(*sign);
+        }
+
+        if (s[0] == ' '){
+            if (reading == 1){
+                return (*num)*(*sign);
+            }
+            *num = recursiveAtoi(s.substr(1, len-1), len-1, sign, num, reading);
+        }
+
+        else if (s[0] == '-'){
+            if (reading == 1){
+                return (*num)*(*sign);
+            }
+            *sign = -1;
+            reading = 1;
+            *num = recursiveAtoi(s.substr(1, len-1), len-1, sign, num, reading);
+        }
+
+        else if (s[0] == '+'){
+            if (reading == 1){
+                return (*num)*(*sign);
+            }
+            *sign = 1;
+            reading = 1;
+            *num = recursiveAtoi(s.substr(1, len-1), len-1, sign, num, reading);
+        }
+
+        else if (s[0] >= '0' && s[0] <= '9'){
+            reading = 1;
+            if (*sign == 1 && ((long)(*num)*10 + (s[0] - '0')) >= INT_MAX){
+                return INT_MAX;
+            }
+            else if (*sign == -1 && ((long)(*num)*10 + (s[0] - '0')) > INT_MAX){
+                return INT_MIN;
+            }
+            *num = (*num)*10 + (s[0] - '0');
+            *num = recursiveAtoi(s.substr(1, len-1), len-1, sign, num, reading);
+            //cout << *num << " sign is " << *sign << endl;
+
+        }
+
+        else{
+            return (*num)*(*sign);
+        }
+
+        return *num;
+    }
+    int myAtoi(string s) {
+        int sign = 1;
+        int num = 0;
+        int reading = 0;
+        return recursiveAtoi(s, s.size(), &sign, &num, reading);
+        return num;
+    }
+};

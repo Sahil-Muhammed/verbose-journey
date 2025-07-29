854eb1821bb7f629fc86cce991cada7837e599d9
Sahil Muhammed <sahilmuhammed156@gmail.com>
2025-07-30 01:49:24 +0530

    Completed a question on climbing stairs using all ways to solve it
    Includes memoization, tabulation, and the space optimized method as well.

--- LeetCode/70.cpp
+++ LeetCode/70.cpp
@@ -37,3 +37,38 @@ public:
         return recursion(n, dp);
     }
 };
+// Attempt 3: Using DP (bottom-up / tabulation)
+// Outcome: Accepted
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        if (n == 1 || n == 2){
+            return n;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; ++i){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+// Attempt 4: Space Optimized DP
+// Outcome: Accepted
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1 || n == 2){
+            return n;
+        }
+        int prev1 = 1, prev2 = 1, curr = 0;
+        for (int i = 2; i <= n; ++i){
+            curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return curr;
+    }
+};
